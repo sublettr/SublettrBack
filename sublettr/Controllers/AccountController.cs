8523802cf0630335ab2bf111e2d8bd3b7994e8f6
Joel Van Auken <posiden104@gmail.com>
2017-10-02 10:40:19 -0400

    user models, account controller

--- sublettr/Controllers/AccountController.cs
+++ sublettr/Controllers/AccountController.cs
@@ -3,36 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using sublettr.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace sublettr.Controllers
 {
     [Route("api/[controller]")]
-    public class SubletController : Controller
+    public class AccountController : Controller
     {
-        // GET api/values
+        // GET: api/values
         [HttpGet]
-        public IEnumerable<SubletModel> Get()
+        public IEnumerable<string> Get()
         {
-            return new SubletModel[] { };
+            return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public SubletModel Get(int id)
+        public string Get(int id)
         {
-            return new SubletModel(1, 1, "Test Sublet");
+            return "value";
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]SubletModel value)
+        public void Post([FromBody]string value)
         {
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]SubletModel value)
+        public void Put(int id, [FromBody]string value)
         {
         }
 
---
+++ sublettr/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace sublettr.Models
+{
+    public class UserModel
+    {
+        public UserModel()
+        { }
+            public int id { get; set; }
+            public string username { get; set; }
+    }
+}
