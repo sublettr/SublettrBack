2299a5f81da8d30bbe0647ce2d55873815ec6826
Joel Van Auken <posiden104@gmail.com>
2017-10-02 11:59:46 -0400

    this sucks

---
+++ sublettr/Data Access/RDSContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace sublettr.DataAccess
+{
+	public class RDSContext : DbContext
+	{
+		public RDSContext()
+		  : base(GetRDSConnectionString())
+		{
+		}
+
+		public static RDSContext Create()
+		{
+			return new RDSContext();
+		}
+	}
+}
