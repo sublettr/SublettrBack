f9c936c2a2b549c161707a5e94a78d608153fc49
Joel Van Auken <posiden104@gmail.com>
2017-10-02 11:01:32 -0400

    full sublet

--- sublettr/Models/FullSubletModel.cs
+++ sublettr/Models/FullSubletModel.cs
@@ -1,28 +1,31 @@
 ﻿using System;
 namespace sublettr.Models
 {
-    public class SubletDataModel
+    public class FullSubletModel
     {
 		
 		public int userId { get; set; }
 		public int id { get; set; }
+        public string address { get; set; }
 		public string description { get; set; }
 		public int roommates { get; set; }
 		public bool isFurnished { get; set; }
         
-        public SubletDataModel(int userId, int id, string description, int roommates, bool isFurnished)
+        public FullSubletModel(int userId, int id, string address, string description, int roommates, bool isFurnished)
         {
             this.userId = userId;
             this.id = id;
+            this.address = address;
             this.description = description;
             this.roommates = roommates;
             this.isFurnished = isFurnished;
         }
 
-        public SubletDataModel(int userId, int id)
+        public FullSubletModel(int userId, int id, string address)
         {
             this.userId = userId;
             this.id = id;
+            this.address = address;
         }
     }
 }
