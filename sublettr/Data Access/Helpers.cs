37386c65477939f27aa5c9840921bb132d8c5ccb
Joel Van Auken <posiden104@gmail.com>
2017-10-02 11:54:43 -0400

    starting aws stuff

---
+++ sublettr/Data Access/Helpers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace sublettr.DataAccess
+{
+
+    public class Helpers
+    {
+		public static string GetRDSConnectionString()
+		{
+			//var appConfig = ConfigurationManager.AppSettings;
+
+			string dbname = appConfig["RDS_DB_NAME"];
+
+			if (string.IsNullOrEmpty(dbname)) return null;
+
+			//string username = appConfig["RDS_USERNAME"];
+			//string password = appConfig["RDS_PASSWORD"];
+			//string hostname = appConfig["RDS_HOSTNAME"];
+			//string port = appConfig["RDS_PORT"];
+
+            
+            string username = "sublettr";
+            string password = "SublettrDB1";
+            //string hostname = appConfig["RDS_HOSTNAME"];
+            //string port = appConfig["RDS_PORT"];
+
+			return "Data Source=" + hostname + ";Initial Catalog=" + dbname + ";User ID=" + username + ";Password=" + password + ";";
+		}
+	}
+}
+
