a46835a108b240ae337a57d5776931016d11a198
Joel Van Auken <posiden104@gmail.com>
2017-10-02 11:07:14 -0400

    update controllers

--- sublettr/Controllers/AccountController.cs
+++ sublettr/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using sublettr.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,33 +12,40 @@ namespace sublettr.Controllers
     [Route("api/[controller]")]
     public class AccountController : Controller
     {
-        // GET: api/values
+        // GET: api/account
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<UserModel> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new UserModel[] {  };
         }
 
-        // GET api/values/5
+        // GET api/account/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public UserModel Get(int id)
         {
-            return "value";
+            return new UserModel(1, "myUserName");
         }
 
-        // POST api/values
+		// GET api/account/full/5
+		[HttpGet("full/{id}")]
+		public UserModel GetFull(int id)
+		{
+			return new UserModel(1, "myUserName");
+		}
+
+        // POST api/account
         [HttpPost]
-        public void Post([FromBody]string value)
+        public void Post([FromBody]UserModel value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/account/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, [FromBody]UserModel value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/account/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
