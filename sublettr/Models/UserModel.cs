848d7b96e9430571421da43fa3a55f6bf9b7acc7
Joel Van Auken <posiden104@gmail.com>
2017-10-02 10:40:19 -0400

    user models, account controller

--- sublettr/Models/UserModel.cs
+++ sublettr/Models/UserModel.cs
@@ -3,6 +3,7 @@ namespace sublettr.Models
 {
     public class UserModel
     {
+<<<<<<< 8b7cee6020520c26d864a5e4f589590568d04c9f
 		
         public int id { get; set; }
 		public string username { get; set; }
@@ -12,5 +13,11 @@ namespace sublettr.Models
             this.id = id;
             this.username = username;
         }
+=======
+        public UserModel()
+        { }
+            public int id { get; set; }
+            public string username { get; set; }
+>>>>>>> user models, account controller
     }
 }
