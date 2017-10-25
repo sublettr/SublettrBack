521c8c0d04922041418b732f35cb63d4de7d57c3
vieck <vieck@purdue.edu>
2017-10-25 10:19:21 -0400

    Added cors, post request works.

--- sublettr/Startup.cs
+++ sublettr/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -34,7 +35,7 @@ namespace sublettr
         {
             // Add framework services.
             services.AddMvc();
-
+	    services.AddCors();
             services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1", new Info
                 {
@@ -67,6 +68,8 @@ namespace sublettr
             app.UseSwaggerUI(c => {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sublettr API V1");
             });
+
+	    app.UseCors(builder => builder.WithOrigins("http://localhost:4200"));
 
             app.UseMvc();
         }
