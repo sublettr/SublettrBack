ad0312b77c4d20be8396fec2d19617b5e44cd48d
Jeremy Lehman <jeremylehman9@gmail.com>
2017-11-03 09:31:37 -0400

    Finish adding Image Urls

---
+++ sublettr/Entities/SubletImageEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace sublettr.Entities
+{
+    public class SubletImageEntity
+    {
+        [ForeignKey("Sublets")]
+        public int SubletID { get; set; }
+        public string ImageUrl { get; set; }
+        
+    }
+}
