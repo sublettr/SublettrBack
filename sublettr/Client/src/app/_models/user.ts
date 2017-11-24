71ac4ba88c6efdbae66722555f0538c574dc4973
Jeremy Lehman <jeremylehman9@gmail.com>
2017-11-24 09:29:48 -0500

    remove front submodule

--- sublettr/Client/src/app/_models/user.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-    email: string;
-    password: string;
-
-    constructor(userData: any) {
-        this.email = userData.email;
-        this.password = userData.password;
-    }
-}
--- sublettr/Client/src/app/_pipes/rating-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RatingPipe } from './rating-pipe.pipe';
-
-describe('RatingPipe', () => {
-  it('create an instance', () => {
-    const pipe = new RatingPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
