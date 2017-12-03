7cfe7709658f1a9d12b741aa9564b0dac332b002
Jeremy Lehman <jwlehman@purdue.edu>
2017-12-03 09:21:50 -0500

    Merge messaging and Client removal (#34)
    * starting
    
    * messaging works
    
    * thread protection
    
    * finihsed i think
    
    * new column in db
    
    * remove frontend submodule

---
+++ sublettr/Entities/MessageEntity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sublettr.Entities
+{
+    public class MessageEntity
+    {
+        public int ID { get; set; }
+        public DateTime SentAt { get; set; }
+        public bool IsRead { get; set; }
+        [ForeignKey("AspNetUsers")]
+        public string AuthorID { get; set; }
+        public string Text { get; set; }
+        [ForeignKey("Threads")]
+        public int ThreadID { get; set; }
+
+        public MessageEntity(int iD, DateTime sentAt, bool isRead, string authorID, string text, int threadID)
+        {
+            ID = iD;
+            SentAt = sentAt;
+            IsRead = isRead;
+            AuthorID = authorID;
+            Text = text;
+            ThreadID = threadID;
+        }
+
+        public MessageEntity()
+        {
+        }
+    }
+}
---
+++ sublettr/Entities/ThreadEntity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sublettr.Entities
+{
+    public class ThreadEntity
+    {
+        public int ID { get; set; }
+        [ForeignKey("Messages")]
+        public int? LastMessageID { get; set; }
+        public string Name { get; set; }
+        public string AvatarSrc { get; set; }
+        [ForeignKey("AspNetUsers")]
+        public string UserAID { get; set; }
+        [ForeignKey("AspNetUsers")]
+        public string UserBID { get; set; }
+
+        public ThreadEntity(int iD, int lastMessageID, string name, string avatarSrc)
+        {
+            ID = iD;
+            LastMessageID = lastMessageID;
+            Name = name;
+            AvatarSrc = avatarSrc;
+        }
+
+        public ThreadEntity()
+        {
+        }
+    }
+}
